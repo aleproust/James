8fe3d6b48b3b25accd9f341ec70e443776f93252
Antoine <Antoine.LEPROUST@canal-plus.com>
2016-12-07 12:02:14 +0100

    Test post message

--- slack/slack-api.js
+++ slack/slack-api.js
@@ -2,19 +2,35 @@
 let Slack = require('slack-node')
 let token = require('../config/config-socket').tokenSlack
 let https = require('https')
+let log = require('../config/log-js')
 let webhookUri = process.env.WEBHOOK_GENERAL
-let slack = new Slack()
+let tokenSlack = process.env.TOKEN_SLACK
+let slack = new Slack(tokenSlack)
 
 slack.setWebhook(webhookUri)
 
-function messageInSlack (text) {
+slack.api("users.list", function(err, response) {
+  log.debug(response.members.length)
+});
+
+
+function messageInSlack(text) {
   slack.webhook({
     channel: '#general',
     username: 'LBJ',
     text: text
-  }, function (err, response) {
+  }, function(err, response) {
     console.log(response)
   })
+}
+
+function postPublicMessage(channel, text) {
+  slack.api('chat.postMessage', {
+    text: text,
+    channel: '#' + channel
+  }, function(err, response) {
+    console.log(response);
+  });
 }
 
 /**
@@ -25,21 +41,9 @@ function messageInSlack (text) {
         "creator": "U024BE7LH"
     }
  */
-function newChannelEvent(channel){
+function newChannelEvent(channel) {
   let newChannelText = `Merci d'avoir crée ce channel, pensez à inviter des personnes qui pourraient être intéressées !`
-  slack.send({
-    channel : `#${channel.name}`,
-    username : 'LBJ',
-    text: newChannelText
-  })
-  .then( result => {
-    slack.send({
-      channel : `#general`,
-      username:'LBJ',
-      text : 'Nouveau channel crée : #'+channel.name
-    })
-  })
-  .catch(err => console.log('Error new channel event ' + err))
+  postPublicMessage(channel.name, newChannelText)
 
 }
 
