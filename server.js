90d04904b33d9f51c30525b3e19bcbdb57cfb1a3
Antoine <Antoine.LEPROUST@canal-plus.com>
2016-12-07 15:53:01 +0100

    Essaie OAuth2.0

--- server.js
+++ server.js
@@ -10,7 +10,19 @@ let express = require('express')
 let routeSlack = require('./routes/slack')
 let slackApi = require('./slack/slack-api')
 let app = express()
+let SlackStrategy = require('passport-slack').Strategy;
+let passport = require('passport');
+const {CLIENT_ID, CLIENT_SECRET} = process.env
+// setup the strategy using defaults
+passport.use(new SlackStrategy({
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET
+  }, (accessToken, refreshToken, profile, done) => {
+    // optionally persist profile data
+  }
+));
 
+app.use(passport.initialize())
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
@@ -25,6 +37,14 @@ app.use(bodyParser.json())
 
 //--- Init routes --
 app.use('/slack', routeSlack)
+// path to start the OAuth flow
+app.get('/auth/slack', passport.authorize('slack'));
+// OAuth callback url
+app.get('/auth/slack/callback',
+  passport.authorize('slack', { failureRedirect: '/login' }),
+  (req, res) => res.redirect('/slack/event')
+);
+
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
